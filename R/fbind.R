7228da088d259779f13dc232c731d67a1c472e20
Cho hyunsun <hyun371579@gmail.com>
2019-07-09 15:26:41 +0200

    change1

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,17 @@
+#' Bind two factors
+#'
+#' Create a new factor from two existing factors, where the new
+#' factor's levels are the union of the levels of the input
+#' factors.
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' fbind(factor(letters[1:3]), factor(letters[26:24]))
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
